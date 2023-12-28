3974581b285962dda1934a494cade07a5631b985
allone9425 <allone9425@gmail.com>
2023-12-28 16:38:39 +0900

    update : 필터링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,32 +1,28 @@
 import { useEffect, useState } from "react";
 import { fetchGetAuctions } from "../api/auction";
 import AuctionList from "../components/Home/AuctionList";
+import CategorySelector from "../components/Home/CategorySelector";
 import { Auction_post, Category } from "../types/databaseRetrunTypes";
+
 const Home = () => {
+  // 경매 데이터 State
   const [auctionData, setAuctionData] = useState<Auction_post[] | null>(null);
+  // 선택된 카테고리 State
+  const [selectedCategories, setSelectedCategories] = useState<Category[]>([]);
+  console.log(selectedCategories);
+  // 쿼리 옵션
   const queryOption = {
     searchKeyword: "",
-    categories: [] as Pick<Category, "category_id">[],
+    categories: selectedCategories,
     limit: 20,
     offset: 0,
     orderBy: "created_at",
     order: false,
   };
 
-  //useEffect(() => {
-  //  (async () => {
-  //    const fetchData = await fetchGetAuctions(
-  //      queryOption.searchKeyword,
-  //      queryOption.categories,
-  //      queryOption.limit,
-  //      queryOption.offset,
-  //      queryOption.orderBy,
-  //      queryOption.order
-  //    );
-  //    setAuctionData(fetchData);
-  //  })();
-  //}, []);
+  // 경매 데이터를 가져오는 useEffect 훅
   useEffect(() => {
+    // 경매 데이터를 비동기적으로 가져오는 함수
     const fetchData = async () => {
       const result = await fetchGetAuctions(
         queryOption.searchKeyword,
@@ -36,15 +32,33 @@ const Home = () => {
         queryOption.orderBy,
         queryOption.order
       );
+      console.log(result);
+      // 가져온 데이터로 State 업데이트
       setAuctionData(result || null);
     };
 
+    // 함수 호출
     fetchData();
-  }, []);
-  console.log(auctionData);
+  }, [selectedCategories]);
+
+  // 카테고리 선택 핸들러
+  const handleCategorySelect = (category: Category) => {
+    console.log(category);
+    setSelectedCategories((prev) => {
+      // 이미 선택된 카테고리를 다시 클릭하면 제거, 아니면 추가
+      if (prev.find((c) => c.category_id === category.category_id)) {
+        return prev.filter((c) => c.category_id !== category.category_id);
+      } else {
+        return [...prev, category];
+      }
+    });
+  };
 
   return (
     <div>
+      {/* 카테고리 선택 컴포넌트 */}
+      <CategorySelector onCategorySelect={handleCategorySelect} />
+      {/* 경매 목록 컴포넌트 */}
       <AuctionList auctions={auctionData} />
     </div>
   );
