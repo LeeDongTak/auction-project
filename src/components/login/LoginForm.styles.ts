13abe89211545855397daa4df647886fd2d61d07
eunbime <ebk1014@gmail.com>
2024-01-03 00:16:01 +0900

    로그인 유효성 검사 및 css 수정

--- src/components/login/LoginForm.styles.ts
+++ src/components/login/LoginForm.styles.ts
@@ -8,12 +8,14 @@ const StFormContainer = styled.div`
   align-items: flex-end;
   margin: auto;
   width: 650px;
-  height: 550px;
+  height: 680px;
 
   h2 {
-    font-size: x-large;
+    font-size: xx-large;
     font-weight: 600;
     margin-bottom: 1.25rem;
+    color: var(--main-color);
+    font-weight: 700;
   }
 `;
 
@@ -28,6 +30,9 @@ const FormWrapper = styled.form`
   background-color: #eee;
   padding: 2rem;
   min-height: 400px;
+  border-radius: 1rem;
+  /* box-shadow: 0 0 8px 5px #222; */
+  background-color: #fff;
 `;
 
 const StInputSection = styled.section`
@@ -35,14 +40,21 @@ const StInputSection = styled.section`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  /* background-color: aliceblue; */
   width: 100%;
+  gap: 1rem;
 
   input {
-    width: 300px;
-    padding: 0.5rem;
-    margin: 0.5rem 0;
-    font-size: medium;
+    width: 400px;
+    padding: 1rem 1.5rem;
+    margin: 0 auto;
+    font-size: large;
+    background-color: #eee;
+    border: none;
+    border-radius: 0.5rem;
+
+    @media (max-width: 650px) {
+      width: 300px;
+    }
   }
 `;
 
@@ -50,24 +62,40 @@ const StButtonSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 1rem;
+  gap: 2rem;
   margin-top: 2rem;
 
   p {
-    font-size: small;
+    font-size: medium;
+    color: var(--main-color);
   }
 
   span {
     cursor: pointer;
-    font-size: medium;
-    font-weight: 500;
+    font-size: large;
+    font-weight: 600;
   }
 `;
 
 const StButton = styled(Button)`
   text-align: center;
-  height: 3rem;
-  width: 300px;
+  height: 4rem;
+  width: 400px;
+  background-color: var(--main-color);
+  opacity: 100%;
+  @media (max-width: 650px) {
+    width: 300px;
+  }
+
+  &:hover {
+    opacity: 90%;
+  }
+`;
+
+const StError = styled.p`
+  color: orange;
+  padding: 0.5rem;
+  font-size: 1.2rem;
 `;
 
 export {
@@ -76,4 +104,5 @@ export {
   StInputSection,
   StButtonSection,
   StButton,
+  StError,
 };
--- src/components/login/SocialLogin/SocialLogin.tsx
+++ src/components/login/SocialLogin/SocialLogin.tsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaGoogle } from "react-icons/fa6";
+import { SiKakaotalk } from "react-icons/si";
 import { styled } from "styled-components";
 import { supabase } from "../../../supabase";
 
@@ -15,8 +17,6 @@ const SocialLogin = () => {
 
     if (error) {
       console.error("로그인 중 오류 발생!", error.message);
-    } else {
-      console.log(data);
     }
   };
 
@@ -33,22 +33,36 @@ const SocialLogin = () => {
 
     if (error) {
       console.error("로그인 중 오류 발생!", error.message);
-    } else {
-      console.log(data);
     }
   };
 
   const onKakaoLoginHandler = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "kakao",
+      options: {
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
     });
+
+    if (error) {
+      console.error("로그인 중 오류 발생!", error.message);
+    }
   };
 
   return (
     <StSocialLoginWrapper>
-      <span onClick={onGoogleLoginHandler}>google</span>
-      <span onClick={onGitHubLoginHandler}>GitHub</span>
-      <span onClick={onKakaoLoginHandler}>Kakao</span>
+      <span onClick={onGoogleLoginHandler}>
+        <FaGoogle />
+      </span>
+      <span onClick={onGitHubLoginHandler}>
+        <FaGithub />
+      </span>
+      <span onClick={onKakaoLoginHandler}>
+        <SiKakaotalk />
+      </span>
     </StSocialLoginWrapper>
   );
 };
@@ -56,7 +70,13 @@ const SocialLogin = () => {
 const StSocialLoginWrapper = styled.section`
   display: flex;
   justify-content: center;
-  gap: 1rem;
+  gap: 4rem;
+  margin-top: 1rem;
+
+  span {
+    font-size: 4rem;
+    color: var(--main-color);
+  }
 `;
 
 export default SocialLogin;
